41761ea2e34a98a13b6242e348496e65248d2415
sutikovi-gor <sutikovi@gmail.com>
2022-02-20 16:21:32 +0200

    Pojehali

---
+++ new.file.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+include 'config.php';
+?>
+
+<html>
+<head>
+	    
+
+<title>Dokumendid</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+</head>
+<body>
+<div style="position:relative; left: 50">
+
+
+<form method="get" action="">
